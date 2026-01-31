cc804ebea9aeb0a733f27ae51e5f53f8cf3f3722
Rodriggo Marcelino <rodriggo.eet@gmail.com>
2026-01-30 23:42:02 -0300

    new commits

--- dags/dag1.py
+++ dags/dag1.py
@@ -1,6 +1,6 @@
 import pendulum
 from airflow import DAG
-from airflow.operators.bash import BashOperator
+from airflow.providers.standard.operators.bash import BashOperator
 
 with DAG(
     dag_id='primeira_dag',
@@ -9,20 +9,20 @@ with DAG(
     start_date=pendulum.datetime(2025, 1, 1, tz="America/Sao_Paulo"),
     catchup=False,
     tags=['curso', 'exemplo'],
-) as dag:    
+) as dag:
     task1 = BashOperator(
         task_id='tsk1',
         bash_command='sleep 5'
     )
-    
+
     task2 = BashOperator(
         task_id='tsk2',
         bash_command='sleep 5'
     )
-    
+
     task3 = BashOperator(
         task_id='tsk3',
         bash_command='sleep 5'
     )
-    
+
     task1 >> task2 >> task3
